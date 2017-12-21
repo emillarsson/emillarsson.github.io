30106b56d2eed39dd47eccc2162b9244e1c48383
Emil Larsson <larsson.emil92@gmail.com>
2017-12-21 13:48:11 +0100

    zoomable

--- space_scripts.js
+++ space_scripts.js
@@ -5,87 +5,116 @@ var noPlanets = 0;
 var moons = [];
 var moonsById = [];
 var noMoons = 0;
-var timeElapsed = Math.random()*300;
+var timeElapsed = Math.random()*30000000000;
+var startScale = 5000000000;
+var pixelScale = startScale;
 
-var Planet = function(name, colour, radius, distance, orbitTime) {
+var strokeColour = '#FFFFFF';
+var Planet = function(name, colour, minRadius, realRadius, distance, orbitTime, ring) {
     this.name = name;
     this.colour = colour;
-    this.radius = radius;
-    this.distance = (smallestScreenSize-10)*distance/4504.3;
+    this.minRadius = minRadius;
+    this.realRadius = realRadius;
+    this.radius = this.realRadius/pixelScale;
+    this.realDistance = distance;
+    this.distance = this.realDistance/pixelScale;
     this.orbitTime = orbitTime;
+    if (ring) this.ring = ring;
     planetsById[noPlanets] = this;
     planets[this.name] = this;
     noPlanets++;
     this.pos = [sunPos[0]+this.distance, sunPos[1]];
-    this.updateDistance = function() {
-        this.distance = (smallestScreenSize-10)*distance/4504.3;
-    }
     this.update = function() {
+        this.radius = this.realRadius/pixelScale;
+        if (this.radius < this.minRadius) this.radius = this.minRadius;
+        this.distance = this.realDistance/pixelScale;
         this.pos = [sunPos[0]+this.distance*Math.cos(2*Math.PI*timeElapsed/this.orbitTime), sunPos[1]+this.distance*Math.sin(2*Math.PI*timeElapsed/this.orbitTime)];
     }
     this.stroke = function() {
         ctxBg.moveTo(sunPos[0]+this.distance, sunPos[1]);
         ctxBg.arc(sunPos[0], sunPos[1], this.distance, 0, 2*Math.PI, false);
-        ctxBg.lineWidth = 0.3;
-        ctxBg.strokeStyle = '#FFFFFF';
+        ctxBg.lineWidth = 0.2;
+        ctxBg.strokeStyle = strokeColour;
     }
     this.fill = function() {
+        if (this.ring) {
+            ctxPlanets.beginPath();
+            ctxPlanets.moveTo(this.pos[0]+this.ring.distance/pixelScale, this.pos[1]);
+            ctxPlanets.arc(this.pos[0], this.pos[1], this.ring.distance/pixelScale, 0, 2*Math.PI, false);
+            ctxPlanets.lineWidth = this.ring.width/pixelScale;
+            ctxPlanets.strokeStyle = '#897852';
+            ctxPlanets.stroke();
+        }
         ctxPlanets.beginPath();
         ctxPlanets.moveTo(this.pos[0], this.pos[1]);
         ctxPlanets.arc(this.pos[0], this.pos[1], this.radius, 0, 2*Math.PI, false);
         ctxPlanets.fillStyle = this.colour;
         ctxPlanets.fill();
+        
+        
     }
 }
 
 function initPlanets() {
-    new Planet('Mercury', '#93837e', 1, 57.9, 0.48);
-    new Planet('Venus','#c8a560', 2, 108.2, 1.23);
-    new Planet('Earth','#5883dc', 2, 149.6, 2);
-    new Planet('Mars','#b66036', 1.5, 227.9, 4);
-    new Planet('Jupiter','#b67862', 10, 778.3, 24);
-    new Moon('Ganymedes', '#888888', planets['Jupiter'], 1,25,20,1);
-    new Moon('Callisto', '#888888', planets['Jupiter'], 1,30,14,1);
-    new Moon('Io', '#888888', planets['Jupiter'], 0.5,18,6,1);
-    new Moon('Europa', '#888888', planets['Jupiter'], 0.5,20,9,1);
+    new Planet('Mercury', '#93837e', 1, 4879, 57909050, 7600522);
+    
+    new Planet('Venus','#c8a560', 2, 12104, 108208000, 19414166);
+    
+    new Planet('Earth','#5883dc', 2, 12756, 149598023, 31536000);
+    new Moon('Moon', '#888888', planets['Earth'], 2, 1737.1, 384399, 2360592, 1);
+    
+    new Planet('Mars','#b66036', 1.5, 6794, 227939134, 59356800);
     
     
-    new Planet('Saturnus','#dcb35b', 8, 1429.4, 58);
-    new Moon('Titan', '#888888', planets['Saturnus'], 1,26,15,1);
+    new Planet('Jupiter','#b67862', 8, 69911, 778567158, 374080032);
+    new Moon('Ganymede', '#888888', planets['Jupiter'], 1,5262.4,1070412,618157,1);
+    new Moon('Callisto', '#888888', planets['Jupiter'], 1,4820.6,1882709,1441929,1);
+    new Moon('Io', '#888888', planets['Jupiter'], 0.5,3660,421700,152850,1);
+    new Moon('Europa', '#888888', planets['Jupiter'], 0.5,3121.6	,671034,306823,1);
     
-    new Planet('Uranus','#5bb6dc', 4, 2871, 168);
-    new Moon('Titania', '#888888', planets['Uranus'], 0.5,18,6,1);
     
-    new Planet('Neptune','#3a5ec4', 4, 4504.3, 330);
-    new Moon('Triton', '#888888', planets['Neptune'], 0.5,18,6,-1);
+    new Planet('Saturn','#dcb35b', 7, 58232, 1433537000, 928959105, {distance: 96040, width:43080});
+    new Moon('Titan', '#888888', planets['Saturn'], 1,26,1377684,1);
+    
+    new Planet('Uranus','#5bb6dc', 5, 51118, 2875031718, 2649670488);
+    new Moon('Titania', '#888888', planets['Uranus'], 0.5,18,752187,1);
+    
+    new Planet('Neptune','#3a5ec4', 5, 49528, 4504449781, 5197132800);
+    new Moon('Triton', '#888888', planets['Neptune'], 0.5,18,507772,-1);
 }
 
-var Moon = function(name, colour, planet, radius, distance, orbitTime, retrograde) {
+var Moon = function(name, colour, planet, minRadius, realRadius, distance, orbitTime, retrograde) {
     this.name = name;
     this.colour = colour;
-    this.radius = radius;
-    this.distance = distance;
+    this.planet = planet;
+    this.minRadius = minRadius;
+    this.realRadius = realRadius;
+    this.radius = this.realRadius/pixelScale;
+    this.realDistance = distance;
+    this.distance = this.realDistance/pixelScale;
     this.orbitTime = orbitTime;
     this.retrograde = retrograde;
     moons[this.name] = this;
     moonsById[noMoons] = this;
     noMoons++;
-    this.pos = [planet.pos[0]+this.distance, planet.pos[1]];
-    this.updateDistance = function() {
-        this.distance = (smallestScreenSize-10)*distance/4504.3;
-    }
+    this.pos = [this.planet.pos[0]+this.distance, this.planet.pos[1]];
     this.update = function() {
-        this.pos = [planet.pos[0]+this.distance*Math.cos(this.retrograde*2*Math.PI*timeElapsed/this.orbitTime), planet.pos[1]+this.distance*Math.sin(this.retrograde*2*Math.PI*timeElapsed/this.orbitTime)];
+        this.radius = this.realRadius/pixelScale;
+        if (this.radius < this.minRadius) this.radius = this.minRadius;
+        this.distance = this.realDistance/pixelScale;
+        this.pos = [this.planet.pos[0]+this.distance*Math.cos(this.retrograde*2*Math.PI*timeElapsed/this.orbitTime), this.planet.pos[1]+this.distance*Math.sin(this.retrograde*2*Math.PI*timeElapsed/this.orbitTime)];
     }
     this.stroke = function() {
+        if (this.distance < this.planet.radius) {return;}
         ctxPlanets.beginPath();
-        ctxPlanets.moveTo(planet.pos[0]+this.distance, planet.pos[1]);
-        ctxPlanets.arc(planet.pos[0], planet.pos[1], this.distance, 0, 2*Math.PI, false);
+        ctxPlanets.moveTo(this.planet.pos[0]+this.distance, this.planet.pos[1]);
+        ctxPlanets.arc(this.planet.pos[0], this.planet.pos[1], this.distance, 0, 2*Math.PI, false);
         ctxPlanets.lineWidth = 0.2;
-        ctxPlanets.strokeStyle = '#FFFFFF';
+        ctxPlanets.strokeStyle = strokeColour;
         ctxPlanets.stroke();
     }
     this.fill = function() {
+        if (this.distance < this.planet.radius) {return;}
         ctxPlanets.beginPath();
         ctxPlanets.moveTo(this.pos[0], this.pos[1]);
         ctxPlanets.arc(this.pos[0], this.pos[1], this.radius, 0, 2*Math.PI, false);
@@ -96,42 +125,54 @@ var Moon = function(name, colour, planet, radius, distance, orbitTime, retrograd
 
 
 var sunPos;
-var smallestScreenSize;
 function setPositions() {
-    
     if (document.body.clientHeight < document.body.clientWidth) {
         smallestScreenSize = document.body.clientHeight/2;
     } else {
         smallestScreenSize = document.body.clientWidth/2;
     }
-    sunPos = [smallestScreenSize, smallestScreenSize];
-    for (var i = 0; i < noPlanets; i++) {
-        planetsById[i].updateDistance(); 
-    }
+    sunPos = [smallestScreenSize+xOffset, smallestScreenSize+yOffset];
 }
 
 function update() {
+    setPositions();
+    pixelScale = startScale/(resolution*smallestScreenSize);
+    ruler.innerHTML = (100*pixelScale).toFixed(0);
     for (var i = 0; i < noPlanets; i++) {
         planetsById[i].update(); 
     }
     for (var j = 0; j < noMoons; j++) {
         moonsById[j].update(); 
     }
+    updateBackground();
 }
 
 function draw() {
     ctxPlanets.clearRect(0, 0, canvasPlanets.width, canvasPlanets.height);
-    for (var i = 0; i < noPlanets; i++) {
-        planetsById[i].fill(); 
-    }
+    ctxPlanets.beginPath();
+    ctxPlanets.moveTo(sunPos[0], sunPos[1]);
+    ctxPlanets.arc(sunPos[0], sunPos[1], 695700/pixelScale, 0, 2*Math.PI, false);
+    ctxPlanets.fillStyle = '#FFFFFF';
+    ctxPlanets.fill();
     for (var j = 0; j < noMoons; j++) {
         moonsById[j].stroke();
         moonsById[j].fill(); 
+    }
+    for (var i = 0; i < noPlanets; i++) {
+        planetsById[i].fill(); 
     }
 
 }
 
 
+function updateBackground() {
+    ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
+    ctxBg.beginPath();
+    for (var i = 0; i < noPlanets; i++) {
+        planetsById[i].stroke(); 
+    }
+    ctxBg.stroke();
+}
 
 function setCanvas() {
     canvasBg = document.getElementById('background');
@@ -139,12 +180,7 @@ function setCanvas() {
     canvasBg.height = smallestScreenSize*2;
     ctxBg = canvasBg.getContext('2d');
     
-    ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
-    ctxBg.beginPath();
-    for (var i = 0; i < noPlanets; i++) {
-        planetsById[i].stroke(); 
-    }
-    ctxBg.stroke();
+    updateBackground();
     
     canvasPlanets = document.getElementById('planets');
     canvasPlanets.width = smallestScreenSize*2;
@@ -155,9 +191,13 @@ function setCanvas() {
 var fps = 0.006;
 
 var canvasBg, canvasPlanets, ctxBg, ctxPlanets;
-var dx = 0;
+var slider, ruler;
+var smallestScreenSize;
+var resolution = 1;
 
 function init() {
+    slider = document.getElementById('timeSlider');
+    ruler = document.getElementById('ruler')
     setCanvas();
     setPositions();
     initPlanets();
@@ -167,18 +207,57 @@ function init() {
 
 init();
 
+var xOffset = 0 
+var yOffset = 0;
+var mouseDown = false;
+var lastDown = [0,0];
+window.onmousedown = function(e) {
+    mouseDown = true;
+    lastDown = [e.screenX, e.screenY];
+}
+window.onmouseup = function(e) {
+    mouseDown = false
+}
+
+window.onmousemove = function(e) {
+    if (mouseDown && e.target.id=='planets') {
+        xOffset += e.screenX - lastDown[0];
+        yOffset += e.screenY - lastDown[1];
+        lastDown = [e.screenX, e.screenY];
+    }
+}
 window.onwheel = function(e) {
-    console.log(e.deltaX);
+    var oldRes = resolution;
+    var zoom = 0.97;
+
+    if (e.deltaY > 0) {
+        zoom = 1/zoom;
+    }
+    resolution *= zoom;
+
+    if (resolution < 1) {
+        resolution = 1;
+        return;
+    }     
+    
+    var dx = (e.layerX-smallestScreenSize-xOffset)*(zoom-1),
+        dy = (e.layerY-smallestScreenSize-yOffset)*(zoom-1);
+    
+    xOffset -= dx;
+    yOffset -= dy;
 }
 
 window.onresize = function(e) {
     setPositions();
     setCanvas();
-    
 }
 
 window.setInterval(function(){
-    timeElapsed += 0.02;
+    timeElapsed += 0.01*slider.value;
     update();
     draw();
-}, 20);
+}, 10);
+
+function print(output) {
+    console.log(output);
+}
